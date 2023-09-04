e30a8c59d08d43f3bf2b5357d225df88ec62fb0c
energizerHim <jinhimchan@naver.com>
2023-09-05 00:58:49 +0900

    책 p.162 게시글 CRUD 완료

---
+++ src/main/java/com/dashboard/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package com.dashboard.springboot.web.dto;
+
+import com.dashboard.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+
+    }
+
+}
